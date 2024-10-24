a6e540d00fb82563ed12d36e0ce70a59366af57e
unknown <shivarajak7337@gmail.com>
2024-10-24 20:51:54 +0530

    This is my first commit

---
+++ CampStr1.java
@@ -0,0 +1,16 @@
+class CampStr1 
+{
+	public static void main(String[] args) 
+	{
+		String s1="ganesh";
+		String s2="rama";
+		if(s1==s2)
+		{
+		System.out.println("equal");
+		}
+		else
+		{
+			System.out.println("unequal");
+		}
+	}
+}
