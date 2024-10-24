174bee5f91626f2c76e1fb7ec7479edef2242fef
unknown <shivarajak7337@gmail.com>
2024-10-24 10:50:58 +0530

    AddDemo

---
+++ AddDemo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class AddDemo 
+{
+	public static void main(String[] args) 
+	{
+		int a,b,c;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter value of a");
+		a=scan.nextInt();
+		System.out.println("Enter value of b");
+		b=scan.nextInt();
+		c=a+b;
+		System.out.println(c);
+
+	}
+}
